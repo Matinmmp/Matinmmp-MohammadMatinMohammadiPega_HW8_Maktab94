d71717a58172a71727c14309d9d651f066d75c42
Matinmmp1381 <matinmmp1381@gmail.com>
2023-04-19 11:53:20 -0700

    fix function

--- HW-1/app.js
+++ HW-1/app.js
@@ -1,4 +1,18 @@
 
+const setterGenerator = (propName) => {
+  return function(value) {
+    this[propName] = value;
+    return this;
+  }
+};
+
+
+
+const user = {}
+const nameSetter = setterGenerator('name')
+const f = nameSetter.bind(user);
+f('Jack');
+console.log(user) // {name: ‘jack’}
 
 
 
