c6a7d10a97fbadbc06d8ffb276b736290202f976
Matinmmp1381 <matinmmp1381@gmail.com>
2023-04-19 11:59:13 -0700

    create counterMaker function

--- HW-3/app.js
+++ HW-3/app.js
@@ -1,4 +1,18 @@
-
+function counterMaker() {
+  let counter = 0;
+  return function (value = 0) {
+    counter += value;
+    return counter;
+  }
+}
+const counter = counterMaker()
+console.log(counter())   // 0
+console.log(counter())   // 0
+console.log(counter(1)) // 1
+console.log(counter())   // 1
+console.log(counter(3)) // 4
+console.log(counter(7)) // 11
+console.log(counter(-5)) // 6
 
 
 
